77f85354448b8ca70847da9de991f1fd3ddd4b72
Goran Ninkovic <goran@goranninkovic.com>
2025-07-14 21:04:34 +0200

    refactor: update assertion in ExampleTest to use explicit comparison
    Closes: PRJ-2

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,16 +1,25 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
 
+/**
+ * @internal
+ *
+ * @coversNothing
+ */
 class ExampleTest extends TestCase
 {
     /**
      * A basic test example.
      */
-    public function test_that_true_is_true(): void
+    public function testThatTrueIsTrue(): void
     {
-        $this->assertTrue(true);
+        // This test is intentionally left empty to demonstrate a basic test structure.
+        // @phpstan-ignore-next-line
+        $this->assertTrue(1 === 1);
     }
 }
